e9e41e86defd09ccea49c24ecc423e2633dc6cb9
Eliot Baker <430383+ebaker@users.noreply.github.com>
2025-02-18 20:59:54 -0800

    feat: create POST /leads console log route

---
+++ src/app/api/leads/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  try {
+    const data = await request.json()
+
+    if (!data.firstname || !data.lastname || !data.email) {
+      return NextResponse.json(
+        { error: 'Missing required fields' },
+        { status: 400 }
+      )
+    }
+    console.log('Received:', data)
+
+    return NextResponse.json(
+      { message: 'Successfully' },
+      { status: 201 }
+    )
+
+  } catch (error) {
+    console.error('Error:', error)
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    )
+  }
+}
